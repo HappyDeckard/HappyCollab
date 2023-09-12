211fbbd26e1e9e81e0fc6f43d4455997e38e9d4a
HappyDeckard <129887370+HappyDeckard@users.noreply.github.com>
2023-09-12 18:54:07 -0300

    Atualização de Teste.cs

---
+++ Assets/Teste.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    void Start()
+    {
+        print("Mensgem do Happy");
+    }
+}
